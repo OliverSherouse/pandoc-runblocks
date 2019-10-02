7762f73840be067d0e5889fb5bf2fd7f1f142126
Oliver Sherouse <oliver@oliversherouse.com>
2019-10-01 22:06:12 -0400

    add tests

--- tests/test_pandoc_runblocks.py
+++ tests/test_pandoc_runblocks.py
@@ -1,5 +1,38 @@
 from pandoc_runblocks import __version__
 
+import subprocess
+
+from pathlib import Path
+
+BASEDIR = Path(__file__).resolve().parent
+SRCDIR = BASEDIR.joinpath("src")
+OUTDIR = BASEDIR.joinpath("out")
+
+
+def get_output_for_path(path):
+    proc = subprocess.run(
+        [
+            "pandoc",
+            "-t",
+            "markdown",
+            "--filter",
+            "pandoc-runblocks",
+            str(path),
+        ],
+        capture_output=True,
+    )
+    return proc.stdout.decode("utf-8")
+
+
+def match_file(filename):
+    got = get_output_for_path(SRCDIR.joinpath(filename))
+    expected = OUTDIR.joinpath(Path(filename).stem + ".md").read_text()
+    assert got == expected
+
 
 def test_version():
     assert __version__ == "0.1.0.dev"
+
+
+def test_python_hello_world():
+    match_file("python_hello_world.md")
